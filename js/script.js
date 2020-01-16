b4da48ecaf7920ce294bf92e4c5247ab33ed3582
Danielle Kellogg <dashkellogg@gmail.com>
2020-01-15 23:19:30 -0500

    Add funcationality to hide/show Other role field

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 //Automatically focs on the first field
 document.getElementById('name').focus();
+const titleOther = document.getElementById('title-other');
+const titleOtherLabel = document.getElementById('title-other-label');
+titleOther.style.display = 'none';
+titleOtherLabel.style.display = 'none';
 
 //”Job Role” section
 //if the Job role field has a value of other
     //show the other text field
     //else hide other text field
 
-    
+const title = document.getElementById('title')
+console.log(title);
+const titleValue = title.value;
+console.log(titleValue);
+
+title.addEventListener('change', (e) => {
+console.log(e);
+    if (e.target.value == 'other') {
+        titleOther.style.display = '';
+        titleOtherLabel.style.display = '';
+    } else {
+        titleOther.style.display = 'none';
+        titleOtherLabel.style.display = 'none';
+    }
+});
+
