9a4146fed509c404e7a0585755709ffcd927a5d1
Danielle Kellogg <dashkellogg@gmail.com>
2020-01-15 22:22:09 -0500

    Autofocus and add optional role field

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Automatically focs on the first field
+document.getElementById('name').focus();
+
+//”Job Role” section
+//if the Job role field has a value of other
+    //show the other text field
+    //else hide other text field
+
+    
