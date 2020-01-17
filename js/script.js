1570feb943bd5e5240c955165b4368e918ce0d76
Danielle Kellogg <dashkellogg@gmail.com>
2020-01-16 22:05:42 -0500

    Add select option to Color field, hides color options on load

--- js/script.js
+++ js/script.js
@@ -28,6 +28,41 @@ console.log(e);
 
 // ”T-Shirt Info” section
 
+    // Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field reads “Please select a T-shirt theme”.
+
+    let color = document.getElementById('color');
+    let colorOptions = color.getElementsByTagName
+    ('option');
+
+    let colorSelect = document.createElement('OPTION');
+    colorSelect.innerHTML = "Please select a T-shirt theme";
+    let firstColor = colorOptions[0];
+    console.log(firstColor);
+    color.insertBefore(colorSelect, firstColor);
+
+    for (i = 0; i < colorOptions.length; i++){   
+    if(i > 0){   
+    colorOptions[i].style.display = 'none';
+    };
+    }
+
+    
+
+    //if design theme is blank
+        // remove all color options
+        // set text to "please select a tshirt"
+
+    //if design theme is JS Puns
+        // display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+        // hide "Tomato," "Steel Blue," and "Dim Grey."
+     // else if design theme is I ♥ JS"
+        // display "Tomato," "Steel Blue," and "Dim Grey."
+        // hide "Cornflower Blue," "Dark Slate Grey," and "Gold."   
+
+    // When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated.
+
+
+
 
 //”Register for Activities” section
 
