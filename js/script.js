e3ebbe4b0bda139413f42f3b062410ed62e4921f
Danielle Kellogg <dashkellogg@gmail.com>
2020-01-24 20:51:36 -0500

    Clean up formatting and comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-//Automatically focs on the first field
+//Automatically focus on the first field
 document.getElementById('name').focus();
 const titleOther = document.getElementById('title-other');
 const titleOtherLabel = document.getElementById('title-other-label');
 titleOther.style.display = 'none';
 titleOtherLabel.style.display = 'none';
 
-//”Job Role” section
-//if the Job role field has a value of other
-    //show the other text field
-    //else hide other text field
+// If "Other" is selected as the Job Role, adds an additional other text field.
 
 const title = document.getElementById('title')
 console.log(title);
@@ -28,89 +25,85 @@ console.log(e);
 
 // T-Shirt: Adds an addition option to the Color field and hides color values until the Design Theme is
 
-    let color = document.getElementById('color');
-    color.placeholder = "test";
-    let colorOptions = color.getElementsByTagName
-    ('option');
+let color = document.getElementById('color');
+color.placeholder = "test";
+let colorOptions = color.getElementsByTagName('option');
+let colorSelect = document.createElement('OPTION');
+colorSelect.innerHTML = "Please select a T-shirt theme";
+let firstColor = colorOptions[0];
+console.log(firstColor);
+color.insertBefore(colorSelect, firstColor);
+colorSelect.selected = true;
 
-    let colorSelect = document.createElement('OPTION');
-    colorSelect.innerHTML = "Please select a T-shirt theme";
-    let firstColor = colorOptions[0];
-    console.log(firstColor);
-    color.insertBefore(colorSelect, firstColor);
-    colorSelect.selected = true;
-
-    for (i = 0; i < colorOptions.length; i++){   
-        if(i > 0){   
+for (i = 0; i < colorOptions.length; i++) {
+    if (i > 0) {
         colorOptions[i].hidden = true;
-        };
-    }
+    };
+}
 
-    let design = document.getElementById('design');
-    design.addEventListener('change', (e) => {
+let design = document.getElementById('design');
+design.addEventListener('change', (e) => {
 
-        for (i = 0; i < colorOptions.length; i++){
-            if (e.target.value == 'js puns') {
-                if (i <= 3){
+    for (i = 0; i < colorOptions.length; i++) {
+        if (e.target.value == 'js puns') {
+            if (i <= 3) {
                 colorOptions[i].hidden = false;
-                } else {
+            } else {
                 colorOptions[i].hidden = true;
-                };
+            };
 
-            } else if (e.target.value == 'heart js') {
-                if (i > 3){
+        } else if (e.target.value == 'heart js') {
+            if (i > 3) {
                 colorOptions[i].hidden = false;
-                } else {
+            } else {
                 colorOptions[i].hidden = true;
-                };
-            }
-        };
-    });
+            };
+        }
+    };
+});
 
-    // When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated.
+// Need to add: When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated.
 
 
-//”Register for Activities” section
+// Need to add: ”Register for Activities” section
 
     // If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available. Reverse this when the activity is unchecked
 
     // As a user selects activities, a running total should display below the list of checkboxes. 
 
 
-//"Payment Info" section
+// Displays payment sections based on the payment option chosen in the select menu.
+let paymentMethod = document.getElementById('payment');
+let selectMethod = document.querySelector('option[value="select method"]');
+selectMethod.disabled = true;
+let creditCard = document.getElementById('credit-card');
+let paypal = document.getElementById('paypal');
+let bitcoin = document.getElementById('bitcoin');
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
 
-    // Display payment sections based on the payment option chosen in the select menu.
-    let paymentMethod = document.getElementById('payment');
-    let selectMethod = document.querySelector('option[value="select method"]');
-    selectMethod.disabled = true;
-    let creditCard = document.getElementById('credit-card');
-    let paypal = document.getElementById('paypal');
-    let bitcoin = document.getElementById('bitcoin');
-    paypal.style.display = 'none';
-    bitcoin.style.display = 'none';
+paymentMethod.addEventListener('change', (e) => {
+    if (e.target.value === "paypal") {
+        console.log('paypal listener works');
+        paypal.style.display = '';
+        bitcoin.style.display = 'none';
+        creditCard.style.display = 'none';
+    } else if (e.target.value === "bitcoin") {
+        console.log('bitcoin listener works');
+        paypal.style.display = 'none';
+        bitcoin.style.display = '';
+        creditCard.style.display = 'none';
+    } else if (e.target.value === "credit card") {
+        console.log('bitcoin listener works');
+        paypal.style.display = 'none';
+        bitcoin.style.display = 'none';
+        creditCard.style.display = '';
+    };
 
-    paymentMethod.addEventListener('change', (e) => {
-        if (e.target.value === "paypal"){
-            console.log('paypal listener works');
-            paypal.style.display = '';
-            bitcoin.style.display = 'none';
-            creditCard.style.display = 'none';
-        } else if (e.target.value === "bitcoin"){
-            console.log('bitcoin listener works');
-            paypal.style.display = 'none';
-            bitcoin.style.display = '';
-            creditCard.style.display = 'none';
-        } else if (e.target.value === "credit card"){
-            console.log('bitcoin listener works');
-            paypal.style.display = 'none';
-            bitcoin.style.display = 'none';
-            creditCard.style.display = '';
-        };
-        
-    });
+});
 
 
-//Form validation
+//Adds Form Validation for Name, Email, Credit Card Number, Zip Code and CVV
 const name = document.getElementById('name');
 const email = document.getElementById('mail');
 const activities = document.getElementById('activities');
@@ -132,8 +125,8 @@ const submit = document.querySelector('button');
 
 name.addEventListener('input', (e) => {
     const regexCheck = /^\s*$/.test(e.target.value);
-        
-    if(regexCheck){
+
+    if (regexCheck) {
         nameError.style.display = '';
         submit.disabled = 'true';
     } else {
@@ -144,8 +137,8 @@ name.addEventListener('input', (e) => {
 
 email.addEventListener('input', (e) => {
     const regexCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e.target.value);
-        
-    if(regexCheck){
+
+    if (regexCheck) {
         mailError.style.display = 'none';
         submit.disabled = 'false';
     } else {
@@ -156,22 +149,21 @@ email.addEventListener('input', (e) => {
 
 cardNumber.addEventListener('input', (e) => {
     const regexCheck = /(^[0-9]{12,15})\d$/.test(e.target.value);
-        
-    if(regexCheck){
+
+    if (regexCheck) {
         ccnumError.style.display = 'none';
         submit.disabled = 'false';
     } else {
         ccnumError.style.display = '';
         submit.disabled = 'true';
-
     }
 });
 
 
 zipCode.addEventListener('input', (e) => {
     const regexCheck = /(^[0-9]{4})\d$/.test(e.target.value);
-        
-    if(regexCheck){
+
+    if (regexCheck) {
         zipError.style.display = 'none';
         submit.disabled = 'false';
     } else {
@@ -182,8 +174,8 @@ zipCode.addEventListener('input', (e) => {
 
 cvv.addEventListener('input', (e) => {
     const regexCheck = /(^[0-9]{2})\d$/.test(e.target.value);
-        
-    if(regexCheck){
+
+    if (regexCheck) {
         cvvError.style.display = 'none';
         submit.disabled = 'false';
     } else {
@@ -193,5 +185,4 @@ cvv.addEventListener('input', (e) => {
 });
 
  //User must select at least one checkbox under the "Register for Activities" section
-
-//Prevent form submission if any validation breaks
+    //Prevent form submission if less than one activity is selected
